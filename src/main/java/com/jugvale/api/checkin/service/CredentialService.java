aa738bba73b2ce2cac043344933837f7171e0817
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2019-11-21 01:03:50 -0200

    created credential service

---
+++ src/main/java/com/jugvale/api/checkin/service/CredentialService.java
@@ -0,0 +1,8 @@
+package com.jugvale.api.checkin.service;
+
+import org.springframework.stereotype.Service;
+
+import com.jugvale.api.checkin.repository.CredentialRepository;
+
+@Service
+public interface CredentialService extends CredentialRepository {}
