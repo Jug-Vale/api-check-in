8059519742ff5e626f8e8d34841eb63a5c7bb0ad
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2019-11-21 01:01:41 -0200

    created main classes

---
+++ src/test/java/com/jugvale/api/checkin/CheckinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jugvale.api.checkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
